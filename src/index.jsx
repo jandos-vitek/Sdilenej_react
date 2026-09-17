472d26e91587c530b6f59d13248b5fc534caf870
Vít Jandoš <jandos@SPSEJECNANET.CZ>
2026-09-17 14:44:56 +0200

    prevedeno na vite

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,5 @@ import './css/index.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.querySelector('#app'));
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
